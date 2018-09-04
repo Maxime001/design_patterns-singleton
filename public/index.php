3ae5b403f982d316101e55c27db6fabe323c79f9
MaximeS <maxime.simet@yahoo.fr>
2018-09-04 11:11:28 +0200

    rightBranch

--- public/index.php
+++ public/index.php
@@ -8,6 +8,9 @@ $config = new App\Config();
 
 // Avoir 1 seule instance d'une classe, avec l'avantage par rapport a une classe statique d'avoir un vrai constructeur ! //
 
+
+// RIGHT BRANCH 
+
 var_dump(App\Config::getInstance());
 var_dump(App\Config::getInstance());
 var_dump(App\Config::getInstance());
