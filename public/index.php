343de3a2ab33ca12e32d66b08a407e96536f68e0
MaximeS <maxime.simet@yahoo.fr>
2018-09-04 11:08:27 +0200

    adding documentation

--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@ require '../app/Autoloader.php';
 
 $config = new App\Config();
 
-// Avoir 1 seule instance d'une classe, avec l'avantage par rapport a une classe statique d'avoir un vrai constructeur ! 
+// Avoir 1 seule instance d'une classe, avec l'avantage par rapport a une classe statique d'avoir un vrai constructeur ! //
 
 var_dump(App\Config::getInstance());
 var_dump(App\Config::getInstance());
